8cc2ffa5b41366700e2868faf2e1d364b1c28ab0
seifeddinhaj <seif1994@yahoo.fr>
2018-12-12 01:38:50 +0100

    popup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AdsProvider } from "../../services/ads";
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 adsliste:any;
-  constructor(public navCtrl: NavController ,private ads: AdsProvider ) {
+  constructor(public navCtrl: NavController ,private ads: AdsProvider, private alertCtrl:AlertController ) {
     this.ads.allAds().subscribe((adsTabel:any)=>
             {
 
                 this.adsliste = adsTabel;
 
         });
+  }
+  showAlert(){
+    const alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
